dc2dc4d5d81ea5a70ee7c3e5da3723c1618ce951
richardg5 <richard.gao5@student.tdsb.on.ca>
2025-03-18 18:39:28 +0000

    Add code

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -4,6 +4,37 @@ public class PracticeProblem {
 
 	}
 
-	
+	public static int find(int[] array, int number) {
+		for (int i = 0; i < array.length; i++) {
+			if (array[i] == number) {
+				return i;
+			}
+		}
+		return -1;
+	}
 
+	public static int findLast(String[] array, String string) {
+		for (int i = array.length - 1; i >= 0; i--) {
+			if (array[i].equals(string)) {
+				return i;
+			}
+		}
+		return -1;
+	}
+
+	public static int findSecond(char[] array, char character) {
+		boolean foundFirst = false;
+		int foundIndex = -1;
+		for (int i = 0; i < array.length; i++) {
+			if (array[i] == character) {
+				if (!foundFirst) {
+					foundFirst = true;
+					foundIndex = i;
+				} else {
+					return i;
+				}
+			}
+		}
+		return foundIndex;
+	}
 }
